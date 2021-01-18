2bc0c92ae916bab0fa475fd16d48e2fcbacf9081
tmes <andmald@yahoo.com>
2021-01-18 14:00:46 -0500

    Fixed README.md, added command line arguments to .py
    Added commandline arguments for server ID, bot token file, and important roles list.  Cleaned up some of the code.

--- README.md
+++ README.md
@@ -5,15 +5,23 @@ A discord bot to remove half of all unimportant roles on a discord server. Usefu
 * [Setup/Dependencies](#setup/dependencies)
 * [TODO](#todo)
 ## General info
-This was created purely for a meme on a discord server I am on. I will be adding to this and making new features, but at the end of the day understand that this is still meme code written by a uni student. Its gonna be scuffed\
+This was created purely for a meme on a discord server I am on. I will be adding to this and making new features, but at the end of the day understand that this is still meme code written by a uni student. Its gonna be scuffed
+
+
 With that warning out of the way, this is just the core functionality. The name of the bot, the profile picture, what it prints back to the server, are all up to you. Go wild in creating your own personal server villain
 ## Setup/Dependencies
 You will need [discord.py](https://github.com/Rapptz/discord.py) and Python v3.5+.
-As of right now you will need manually add the server ID and bot token to the .py file. I will be adding ways to simply pass these along in the command line in the future. Any server roles that need to be preserved such as moderation roles can also be added to the .py by listing their ids in a .txt file and hard coding it (working on having this not be hard coded as well)\
-After all of that, simply run ThanosDiscordBot.py.\
+
+
+You will need to have your bot token, and important roles ready in a txt file. The server ID can be added directly into the command line
+
+
+The syntax to run this is ThanosDiscordBot.py SERVER_ID BOT_TOKEN_FILE [IMPORTANT_ROLES_FILE]
+
+
 If you end up getting a SSL certificate error, it means that you either need to update your OS or manually update Discords SSL certificate on your computer.
 ## TODO
-* Have the server ID not be hard coded into the .py file
+* Need to add input validation for the command line arguments
 * Fix this mess of a naming scheme (honestly just clean up this code in general)
 * Allow important server roles to be added via discord command rather than hard coded IDs
 * Other new functions
--- ThanosDiscordBot.py
+++ ThanosDiscordBot.py
@@ -1,28 +1,18 @@
 import discord
 from discord.ext import commands
+import sys
 import random
+import asyncio
 
 description = 'A discord bot to remove half of all unimportant roles on a discord server. Useful and fun way to clean up an excessive amount of meme roles in a server'
 intents = discord.Intents.default()
-# ENTER SERVER ID HERE
+importantRoles = None
+botToken = 0
 GUILD_ID = 0
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
-def removeImportantRolesFromList(oldRoles):
-    newRoles = oldRoles
-    #ADD IMPORTANT SERVER ROLE IDS HERE
-    with open("") as file:
-        importantRoles = file.read().splitlines()
-
-    x = 0
-    while x < len(newRoles):
-        if str(newRoles[x].id) in importantRoles:
-            del newRoles[x]
-        x = x + 1
-    return newRoles
-
-def randomlyRemoveRoles(preSnapRoles):
+def randomlyRemoveRolesFromList(preSnapRoles):
     newRoles = []
     numberToBeSnapped = len(preSnapRoles) // 2
     for x in range(numberToBeSnapped):
@@ -31,6 +21,28 @@ def randomlyRemoveRoles(preSnapRoles):
         del preSnapRoles[randomNumber]
     return newRoles
 
+
+def removeImportantRolesFromList(oldRoles):
+    newRoles = oldRoles
+    x = 0
+    while x < len(newRoles):
+        if str(newRoles[x].id) in importantRoles:
+            del newRoles[x]
+        x = x + 1
+    return newRoles
+
+
+def removeImportantRoles(guild):
+    if importantRoles is not None:
+        return removeImportantRolesFromList(guild.roles)
+    else:
+        return guild.roles
+
+
+def invalidSyntax():
+    print("Invalid syntax, enter -h to receive more information")
+    sys.exit(0)
+
 @bot.event
 async def on_ready():
     print('Logged in as: ')
@@ -38,12 +50,14 @@ async def on_ready():
     print(bot.user.id)
     print('-------')
 
+
 @bot.command()
 async def thanos(ctx, arg):
+    guild = bot.get_guild(GUILD_ID)
+
     if arg == 'auto_snap':
-        guild = bot.get_guild(GUILD_ID)
-        originalRoles = removeImportantRolesFromList(guild.roles)
-        survivingRoles = randomlyRemoveRoles(originalRoles)
+        originalRoles = removeImportantRoles(guild)
+        survivingRoles = randomlyRemoveRolesFromList(originalRoles)
 
         for x in range(len(originalRoles)):
             if originalRoles[x] in survivingRoles:
@@ -60,20 +74,35 @@ async def thanos(ctx, arg):
             await ctx.send(survivingRoles[x].name)
 
     elif arg == 'snap':
-        guild = bot.get_guild(GUILD_ID)
-        originalRoles = removeImportantRolesFromList(guild.roles)
-        survivingRoles = randomlyRemoveRoles(originalRoles)
+        originalRoles = removeImportantRoles(guild)
+        survivingRoles = randomlyRemoveRolesFromList(originalRoles)
 
         await ctx.send("The following roles have recieved Thanos\' mercy: ")
         for x in range(len(survivingRoles)):
             await ctx.send(survivingRoles[x].name)
-
     elif arg == 'help':
         await ctx.send("Everything seems to have loaded in correctly! Thanos is ready to restore balance. Use snap to list out the roles, and auto_snap to automatically remove them")
 
-# ENTER BOT TOKEN HERE
-with open("bot_token.txt") as reader:
-    botToken = reader.read()
+if len(sys.argv) == 2:
+    if sys.argv[1] in {'-h','-help','help'}:
+        print("Syntax for ThanosDiscordBot is as follows: ThanosDiscordBot.py SERVER_ID BOT_TOKEN_FILE [IMPORTANT_ROLES_FILE]")
+        sys.exit(0)
+    else:
+        invalidSyntax()
+elif len(sys.argv) == 3:
+    GUILD_ID = int(sys.argv[1])
+    with open(sys.argv[2]) as reader:
+        botToken = reader.read()
+elif len(sys.argv) == 4:
+    GUILD_ID = int(sys.argv[1])
+    with open(sys.argv[2]) as reader:
+        botToken = reader.read()
+    with open(sys.argv[3]) as file:
+        importantRoles = file.read().splitlines()
+else:
+    invalidSyntax()
+
+# Need to write in proper input validation
 
 if botToken != '':
     bot.run(botToken)
